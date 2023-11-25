5e62de1054f9591da764b2a6296d5d07fa37877f
Dany-Christopher <perrierechristopher@gmail.com>
2023-11-25 21:09:41 +0000

    functional tests implemented

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,61 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+    test("Convert a valid input such as 10L: GET request to /api/convert", function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/convert?input=10L")
+          .end(function (err, res) {
+            const responseObject = JSON.parse(res.text)
+            assert.equal(2.64172 ,responseObject.returnNum);
+            done();
+          });
+      });
+
+      test("Convert an invalid input such as 32g: GET request to /api/convert", function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/convert?input=32g")
+          .end(function (err, res) {
+            assert.equal("invalid unit" , res.text);
+            done();
+          });
+      });
+
+      test("Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert", function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/convert?input=3/7.2/4kg")
+          .end(function (err, res) {
+            assert.equal("invalid number" , res.text);
+            done();
+          });
+      });
+
+      test("Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert", function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/convert?input=3/7.2/4kilomegagram")
+          .end(function (err, res) {
+            assert.equal("invalid number and unit" , res.text);
+            done();
+          });
+      });
+
+      test("Convert with no number such as kg: GET request to /api/convert", function (done) {
+        chai
+          .request(server)
+          .keepOpen()
+          .get("/api/convert?input=kg")
+          .end(function (err, res) {
+            const requestObject = JSON.parse(res.text)
+            assert.equal(1, requestObject.initNum);
+            done();
+          });
+      });
+    
 });
