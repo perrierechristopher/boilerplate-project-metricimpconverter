27ad678f04511e64600dc174a85afe62278266e3
Dany-Christopher <perrierechristopher@gmail.com>
2023-11-25 16:49:27 +0000

    Api route successfully implemented

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,49 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    const { input } = req.query;
+    let errorString = "";
+
+    if (input) {
+      let initNum = convertHandler.getNum(input);
+      let initUnit = convertHandler.getUnit(input);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let string = convertHandler.getString(
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit
+      );
+
+
+      if (initNum instanceof Error && initUnit instanceof Error) {
+        errorString = "invalid number and unit";
+      } else if (initNum instanceof Error) {
+        errorString = "invalid number";
+      } else if (initUnit instanceof Error) {
+        errorString = "invalid unit";
+      }
+
+      if (errorString.length > 0) {
+        return res.send(errorString)
+      } else {
+        return res.json({
+          initNum,
+          initUnit,
+          returnNum,
+          returnUnit,
+          string,
+        });
+      }
+    } else {
+      return res.status(400).send("invalid input");
+    }
+  });
 };
